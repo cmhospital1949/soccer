ffdc34fa0382c13d87378aa1e53818eb78ddebe0
cmhospital <spine_doc@naver.com>
2025-06-18 12:20:48 +0900

    Load Phaser from CDN

---
+++ js/objects/Player.js
@@ -0,0 +1,9 @@
+export default class Player extends Phaser.GameObjects.Ellipse {
+  constructor(scene, x, y, tint = 0xff0000) {
+    super(scene, x, y, 32, 32, tint);
+    scene.add.existing(this);
+    this.setOrigin(0.5);
+    this.setScale(0.5);
+    this.hasBall = false;
+  }
+}
---
+++ js/scenes/GameScene.js
@@ -0,0 +1,39 @@
+import Field from '../objects/Field.js';
+import Player from '../objects/Player.js';
+import Ball from '../objects/Ball.js';
+import InputManager from '../managers/InputManager.js';
+import TacticsManager from '../managers/TacticsManager.js';
+
+export default class GameScene extends Phaser.Scene {
+  constructor() {
+    super('GameScene');
+  }
+
+  create() {
+    const cfg = this.cache.json.get('gameConfig').field;
+    this.field = new Field(this, cfg.width, cfg.height);
+
+    const formation = this.cache.json.get('formations').formations[0];
+    const basePositions = formation.positions;
+
+    this.players = [];
+    basePositions.forEach(pos => {
+      this.players.push(new Player(this, pos.x, pos.y, 0xff0000));
+    });
+    basePositions.forEach(pos => {
+      this.players.push(new Player(this, cfg.width - pos.x, pos.y, 0x0000ff));
+    });
+
+    this.ball = new Ball(this, basePositions[0].x, basePositions[0].y);
+    this.players[0].hasBall = true;
+
+    this.inputManager = new InputManager(this, this.players, this.ball);
+    this.inputManager.enable();
+
+    this.tacticsManager = new TacticsManager(this, this.ball);
+  }
+
+  update() {
+    this.tacticsManager.update();
+  }
+}
