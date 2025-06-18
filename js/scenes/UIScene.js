8e3ed6bb6481b61abdc9484e4a71397e34584140
cmhospital <spine_doc@naver.com>
2025-06-18 13:12:08 +0900

    Refine conflict policy and fix menu click

---
+++ js/scenes/UIScene.js
@@ -0,0 +1,16 @@
+export default class UIScene extends Phaser.Scene {
+  constructor() {
+    super('UIScene');
+  }
+
+  create(data) {
+    this.gameScene = data.gameScene;
+    this.scoreText = this.add
+      .text(400, 20, '0 : 0', { fontSize: '24px', color: '#ffffff' })
+      .setOrigin(0.5);
+  }
+
+  updateScore(a, b) {
+    this.scoreText.setText(`${a} : ${b}`);
+  }
+}
